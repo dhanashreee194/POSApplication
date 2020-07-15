d3cb9e41aef9fb633ffc26f36a956e7f96665fac
dhanashreee194 <dhanashree194@gmail.com>
2020-07-15 19:00:11 +0530

    Screen changes

---
+++ app/src/main/java/com/example/posapplication/Home.java
@@ -0,0 +1,19 @@
+package com.example.posapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        // Set a Toolbar to replace the ActionBar.
+
+
+
+    }
+}
